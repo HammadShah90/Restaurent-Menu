0ee6773eb440e44bad019bcc9f2a2c72df906827
123alishakan <alishakhan3564@gmail.comn>
2023-04-26 10:47:07 +0500

    initial period

---
+++ app.js
@@ -0,0 +1,11 @@
+var mainBtns = ['All', 'Breakfast', 'Lunch', 'Shakes', 'Dinner'];
+
+var mainButtons = document.querySelector(".mainButtons");
+
+console.log(mainButtons);
+
+var mainButtonContent = []
+
+mainBtns.forEach((btn) => {
+    var string = '<button>All</button>';
+})
