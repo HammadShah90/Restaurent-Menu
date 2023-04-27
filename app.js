c13471a94813069eb5e97b6772c3c6820b8b7ecf
HammadShah <121276577+HammadShah90@users.noreply.github.com>
2023-04-28 00:41:50 +0500

    functionality

--- app.js
+++ app.js
@@ -1,11 +1,76 @@
-var mainBtns = ['All', 'Breakfast', 'Lunch', 'Shakes', 'Dinner'];
+// ---------------Menu Buttons-----------------
 
-var mainButtons = document.querySelector(".mainButtons");
+const menuBtns = ['All', 'Broast', 'Burgers', 'Bar B Q', 'Rolls', 'Soft Drinks'];
+// console.log(menuBtns);
 
-console.log(mainButtons);
+const menuButtons = document.querySelector(".menuButtons");
+// console.log(menuButtons);
 
-var mainButtonContent = []
+let menuButtonContent = [];
 
-mainBtns.forEach((btn) => {
-    var string = '<button>All</button>';
+menuBtns.forEach((btns) => {
+    let categoryBtns = `<button class="mainBtns">${btns}</button>`;
+    // console.log(categoryBtns);
+
+    menuButtonContent.push(categoryBtns);
+    // console.log(menuButtonContent);
+});
+
+menuButtons.innerHTML = menuButtonContent.join("");
+// console.log(menuButtons);
+
+
+// ---------------Menu Items List-----------------
+
+const menuItemsList = [
+    {
+        id: 1,
+        category: "broast",
+        image: "./Assets/Chicken-Broast.jpg",
+        itemName: "Chicken Broast Qtr",
+        price: "Rs:380",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vel ad alias corrupti temporibus."
+    },
+    {
+        id: 1,
+        category: "broast",
+        image: "./Assets/Chicken-Broast.jpg",
+        itemName: "Chicken Broast Qtr",
+        price: "Rs:380",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vel ad alias corrupti temporibus."
+    },
+    {
+        id: 1,
+        category: "broast",
+        image: "./Assets/Chicken-Broast.jpg",
+        itemName: "Chicken Broast Qtr",
+        price: "Rs:380",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vel ad alias corrupti temporibus."
+    },
+];
+// console.log(menuItemsList[0].image);
+
+const menuItems = document.querySelector(".itemsSection");
+// console.log(menuItems);
+
+let menuItemListContent = [];
+
+menuItemsList.forEach((items) => {
+    let itemsList = `<img src="${items.image}" alt="">
+    <div>
+        <div>
+            <h4>${items.itemName}</h4>
+            <h4>${items.price}</h4>
+        </div>
+        <p>
+            ${items.description}
+        </p>
+    </div>`;
+    // console.log(itemsList);
+
+    menuItemListContent.push(itemsList);
+    // console.log(menuItemListContent);
 })
+
+menuItems.innerHTML = menuItemListContent.join("");
+console.log(menuItems);
